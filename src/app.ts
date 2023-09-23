0e88752058232dae7db0574687051ca7345b5d6c
kujtimprenku <prenkukujtim.kp@gmail.com>
2023-09-23 21:35:42 +0200

    Render the form in DOM.

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,20 @@
-// Code goes here!
-console.log("test")
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDialogElement;
+    element: HTMLFormElement;
+    constructor() {
+        this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+        this.hostElement = document.getElementById("app")! as HTMLDialogElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = "user-input"
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element)
+    }
+}
+
+const projectInput = new ProjectInput();
